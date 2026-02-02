0213e37006016a9cad32aa4c128e1b5bee57bd41
StarryRing-Roblox <doghwrf@gmail.com>
2026-02-02 17:06:06 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,24 +1,11 @@
---[[
-  /$$$$$$  /$$                                     /$$ /$$       /$$   /$$           /$$      
- /$$__  $$| $$                                    | $$| $$      | $$  | $$          | $$      
-| $$  \ $$| $$$$$$$  /$$   /$$  /$$$$$$$  /$$$$$$ | $$| $$      | $$  | $$ /$$   /$$| $$$$$$$ 
-| $$$$$$$$| $$__  $$| $$  | $$ /$$_____/ |____  $$| $$| $$      | $$$$$$$$| $$  | $$| $$__  $$
-| $$__  $$| $$  \ $$| $$  | $$|  $$$$$$   /$$$$$$$| $$| $$      | $$__  $$| $$  | $$| $$  \ $$
-| $$  | $$| $$  | $$| $$  | $$ \____  $$ /$$__  $$| $$| $$      | $$  | $$| $$  | $$| $$  | $$
-| $$  | $$| $$$$$$$/|  $$$$$$$ /$$$$$$$/|  $$$$$$$| $$| $$      | $$  | $$|  $$$$$$/| $$$$$$$/
-|__/  |__/|_______/  \____  $$|_______/  \_______/|__/|__/      |__/  |__/ \______/ |_______/ 
-                     /$$  | $$                                                                
-                    |  $$$$$$/                                                                
-                     \______/  
-]]
 
 getgenv().AbysallHubSettings = {
-	Name = "Abysall Hub",
-	DiscordInvite = "https://discord.gg/DXJNkSwje3",
+	Name = "MR-S",
+	DiscordInvite = "http://qins.mc.hi.cn/MR-S",
 	SelectedLibrary = "Obsidian",
 	GameName = "Placeholder",
-	ExecutorSupport = loadstring(game:HttpGet(AbysallHubSettings.Repository .. "/Components/ExecutorSupport.lua"))()
-	Repository = "https://raw.githubusercontent.com/bocaj111004/AbysallHubNew/refs/heads/main/"
+	ExecutorSupport = loadstring(game:HttpGet(AbysallHubSettings.Repository .. "ExecutorSupport.lua"))()
+	Repository = "https://github.com/doghwrf001/111/raw/refs/heads/222/"
 }
 local LocalPlayer = game:GetService("Players").LocalPlayer
 local RequiredFunctions = {"writefile", "delfile", "readfile", "isfile", "listfiles", "makefolder", "delfolder", "isfolder", "cloneref"}
@@ -29,12 +16,12 @@ if not AbysallHubSettings.ExecutorSupport[Name] then
   end
 end
 if ExecutorSupported == false then
-  LocalPlayer:Kick("Your executor doesn't support Abysall Hub.\nTry looking for executors on trustworthy sites such as whatexpsare.online or voxlis.net!")
+  LocalPlayer:Kick("Your executor doesn't support MR-S\nTry looking for executors on trustworthy sites such as whatexpsare.online or voxlis.net!")
   return
 end
-local LoaderData = loadstring(game:HttpGet(AbysallHubSettings.Repository .. "/Misc/LoaderData.lua"))()
-local NameData = loadstring(game:HttpGet(AbysallHubSettings.Repository .. "/Misc/LoaderData.lua"))()
-local ESP = loadstring(game:HttpGet(AbysallHubSettings.Repository .. "/Components/ESP.lua"))()
+local LoaderData = loadstring(game:HttpGet(AbysallHubSettings.Repository .. "LoaderData.lua"))()
+local NameData = loadstring(game:HttpGet(AbysallHubSettings.Repository .. "LoaderData.lua"))()
+local ESP = loadstring(game:HttpGet(AbysallHubSettings.Repository .. "ESP.lua"))()
 local SelectedLoader = LoaderData[game.GameId] or LoaderData[0]
 AbysallHubSettings.GameName = NameData[SelectedLoader]
-loadstring(game:HttpGet(AbysallHubSettings.Repository .. "Places/" .. SelectedLoader .. "/Loader.lua"))()
+loadstring(game:HttpGet(AbysallHubSettings.Repository .. "MR-S-1/" .. SelectedLoader .. "/Loader.lua"))()
